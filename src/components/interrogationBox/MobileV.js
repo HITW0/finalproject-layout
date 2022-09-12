8e39d72effc0dd06380722df75567b1521fc43ad
Ahmad Alsoobeh <soobeh26@gmail.com>
2022-09-12 10:22:35 +0200

    frame

---
+++ src/components/interrogationBox/MobileV.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './mobileV.css'
+
+function MobileView() {
+    return (
+        <>
+            <div class="background">
+                <div class="content">INTERROGATION</div>
+            </div>
+            <div className='container'>
+
+                <div className='leftDiv'>
+
+                </div>
+
+                <div className='rightDiv'>
+
+                </div>
+
+            </div>
+        </>
+    );
+}
+export default MobileView;
